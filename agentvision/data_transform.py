08cd3522c9bfaab0caea4bdbee3c6c52a8f4b0b7
Arun Kumar RN <arunkumar.rn@enquero.com>
2020-09-26 19:22:55 +0530

    added mean and std

--- agentvision/data_transform.py
+++ agentvision/data_transform.py
@@ -16,6 +16,8 @@ class CIFAR10Transforms:
         pass
 
     def build_transforms(self,  train_tfms_list=[], test_tfms_list=[]):
-        train_tfms_list.extend([A.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]), ToTensorV2()])
-        test_tfms_list.extend([A.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]), ToTensorV2()])
+        mean = (0.4914, 0.4822, 0.4465)
+        std = (0.2023, 0.1994, 0.2010)
+        train_tfms_list.extend([A.Normalize(mean=mean, std=std), ToTensorV2()])
+        test_tfms_list.extend([A.Normalize(mean=mean, std=std), ToTensorV2()])
         return A.Compose(train_tfms_list), A.Compose(test_tfms_list)
